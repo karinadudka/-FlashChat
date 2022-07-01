cf2ce707b459e43185f8c14524e37112f6aeb9c2
karinadudka <62517663+karinadudka@users.noreply.github.com>
2022-07-01 19:58:39 +0300

    Flash chat Messenger
    Flash chat Messenger

---
+++ Flash Chat iOS13/Controllers/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Карина Дудка on 15.06.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
---
+++ Podfile
@@ -0,0 +1,20 @@
+
+platform :ios, '9.0'
+
+target 'Flash Chat iOS13' do
+ 
+  use_frameworks!
+
+  # Pods for Flash Chat iOS13
+  
+pod 'FirebaseAuth'
+pod 'FirebaseFirestore'
+
+
+
+inhibit_all_warnings!
+
+
+
+
+end
